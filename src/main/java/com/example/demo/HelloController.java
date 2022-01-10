58da26ef0e1f72dca3644e4be8c011704f8d9482
OhHaneol <otyvs1109@hs.ac.kr>
2022-01-10 21:43:01 +0900

    :sparkles: annotation

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello() {
+        System.out.println("hello 안녕");
+        return "hello";
+    }
+}
