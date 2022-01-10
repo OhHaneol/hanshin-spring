a1b665f9b20ed7212cf5d032db0d20a94cede1f2
OhHaneol <otyvs1109@hs.ac.kr>
2022-01-10 22:28:18 +0900

    :sparkles: 타임리프와 html

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String hello() {
-        System.out.println("hello 안녕");
+    public String hello(Model model) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "이름");
+        map.put("age", 19);
+        map.put("tel", "010-1111-2222");
+
+        model.addAttribute("data", map);
         return "hello";
     }
 }
