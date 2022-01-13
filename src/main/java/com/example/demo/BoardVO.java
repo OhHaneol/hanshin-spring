5d042ae6a98cc33b2ca590f873de5a142882ba78
OhHaneol <otyvs1109@hs.ac.kr>
2022-01-13 19:12:53 +0900

    :sparkles: ch.3-3 Annotation Part2

---
+++ src/main/java/com/example/demo/BoardVO.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class BoardVO {
+    private String title;
+    private String content;
+
+    public BoardVO(String title, String content) {
+        this.title=title;
+        this.content=content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
