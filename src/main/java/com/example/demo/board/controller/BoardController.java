c72d0aef3b008d3d9da04d4d88d9e320426bfee6
OhHaneol <otyvs1109@hs.ac.kr>
2022-01-15 17:05:49 +0900

    :sparkles: ch.3-4 파라미터 적용

--- src/main/java/com/example/demo/board/controller/BoardController.java
+++ src/main/java/com/example/demo/board/controller/BoardController.java
@@ -1,24 +1,73 @@
 package com.example.demo.board.controller;
 
+import com.example.demo.board.controller.model.BoardVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
     @RequestMapping("/list")
-    public String boardList() {
+    public String boardList(
+            Model model
+    ) {
+
+        ArrayList<BoardVO> list = new ArrayList<>();
+
+        for(int i=0;i<=10;i++) {
+            //  객체 생성
+            BoardVO boardVO = generate(i,i+"번 게시물", i + "번 게시물 내용", "관리자", 100);
+            //  생성한 객체 ArrayList에 넣기(10번~)
+            list.add(boardVO);
+        }
+
+        model.addAttribute("result", list);
+
         return "board/list";
     }
 
-    @RequestMapping("/view")
-    public String boardView() {
-        return "board/view";
+    @RequestMapping("/mainView")
+    public String boardView(
+            @RequestParam(value = "id", defaultValue = "1") int id,
+            Model model
+    ) {
+
+        ArrayList<BoardVO> list = new ArrayList<>();
+
+        for(int i=0;i<=10;i++) {
+            //  객체 생성
+            BoardVO boardVO = generate(i,i+"번 게시물", i + "번 게시물 내용", "관리자", 100);
+            //  생성한 객체 ArrayList에 넣기(10번~)
+            list.add(boardVO);
+        }
+
+        model.addAttribute("result", list.get(id));
+
+        return "board/mainView";
     }
 
     @RequestMapping("/write")
-    public String boardWrite() {
+    public String boardWrite(Model model) {
         return "board/write";
     }
+
+    public BoardVO generate(int id, String title, String content, String writer, int count) {
+        BoardVO boardVO = new BoardVO();
+
+        boardVO.setId(id);
+        boardVO.setTitle(title);
+        boardVO.setContent(content);
+        boardVO.setWriter(writer);
+        boardVO.setRegDate(LocalDateTime.of(2021,1,1,12,30,30));
+        boardVO.setCount(count);
+
+        return boardVO;
+    }
+
 }
---
+++ src/main/java/com/example/demo/board/controller/model/BoardVO.java
@@ -0,0 +1,71 @@
+package com.example.demo.board.controller.model;
+
+import java.time.LocalDateTime;
+
+public class BoardVO {
+    private int id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime regDate;
+    private int count;
+
+    public BoardVO() {}
+
+    public BoardVO(int id, String title, String content, String writer, LocalDateTime regDate, int count) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.regDate = regDate;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public LocalDateTime getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDateTime regDate) {
+        this.regDate = regDate;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
