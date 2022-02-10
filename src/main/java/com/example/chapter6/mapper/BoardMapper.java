ac8561fcec338581ef9c89007c2d1315875ad1d2
OhHaneol <otyvs1109@hs.ac.kr>
2022-02-11 07:39:01 +0900

    :sparkles: chapter 7 게시글 수정

--- src/main/java/com/example/chapter6/mapper/BoardMapper.java
+++ src/main/java/com/example/chapter6/mapper/BoardMapper.java
@@ -3,8 +3,16 @@ package com.example.chapter6.mapper;
 import com.example.chapter6.model.BoardVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BoardMapper {
+
+    List<BoardVO> selectBoardVO();
+
+    BoardVO selectBoardVOById(int id);
+
+    void updateBoardVO(BoardVO boardVO);
 
     void insertBoardVO(BoardVO boardVO);
 }
