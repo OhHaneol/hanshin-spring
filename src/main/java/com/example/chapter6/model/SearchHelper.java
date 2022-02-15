2566d097b3ed2fd2b2a5ffb2d0f6538e799c8b2a
OhHaneol <otyvs1109@hs.ac.kr>
2022-02-16 00:00:19 +0900

    :sparkles: chapter 7 페이지

--- src/main/java/com/example/chapter6/model/SearchHelper.java
+++ src/main/java/com/example/chapter6/model/SearchHelper.java
@@ -14,7 +14,16 @@ public class SearchHelper {
     private int srchCode = 1000;
     private String srchType;
     private String srchKeyword;
-    private int page = 1;
+
+    // 한 페
     private int startIndex = 0;
+    // 한 페이지에 출력될 row 개수
     private int pageSize = 10;
+    // 한 페이지에 페이징 번호가 몇 개까지 출력되게 할 것인지
+    private int blockSize = 10;
+    // 현재 페이지 번호
+    private int page = 1;
+    // 현재 블럭 번호
+    private int block = 1;
+
 }
